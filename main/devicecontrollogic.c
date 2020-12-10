23db85368bf47f836e178ecb2db67f9bcd82e071
--global <afa@afa.moe>
2020-12-10 23:54:36 +0800

    refactor for better naming

--- main/devicecontrollogic.c
+++ main/devicecontrollogic.c
@@ -46,30 +46,29 @@ typedef struct device_control_config_t {
     uint body_detection_delay_seconds;
 } device_control_config;
 
-static device_control_config config;
+static device_control_config _config;
 
-static bool body_detected = false;
-xQueueHandle device_control_event_queue;
-TimerHandle_t body_detection_delay_timer;
-nvs_handle nvs_config_handle;
-
-static TickType_t body_detection_delay_timer_delay;
+static bool _body_detected = false;
+static xQueueHandle _device_control_event_queue;
+static TimerHandle_t _body_detection_delay_timer;
+static nvs_handle _nvs_config_handle;
+static TickType_t _body_detection_delay_timer_delay;
 
 
 inline static void write_nvs_config_body_detection_enabled(bool enabled)
 {
-    nvs_set_u8(nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_ENABLED, enabled);
+    nvs_set_u8(_nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_ENABLED, enabled);
 }
 
 inline static void write_nvs_config_body_detection_delay(uint delay)
 {
-    nvs_set_u32(nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_DELAY, delay);
+    nvs_set_u32(_nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_DELAY, delay);
 }
 
 void body_detection_delay_timeout(TimerHandle_t xTimer)
 {
     UNUSED(xTimer);
-    body_detected = false;
+    _body_detected = false;
     ESP_LOGI(LOG_TAG_DEVICE_CONTROL, "body detection delay timed out");
 }
 
@@ -79,33 +78,33 @@ static void device_control_task(void* arg)
     for(;;) {
         heap_caps_check_integrity_all(true);
         device_control_event event = {};
-        if(xQueueReceive(device_control_event_queue, &event, portMAX_DELAY)) {
+        if(xQueueReceive(_device_control_event_queue, &event, portMAX_DELAY)) {
             switch (event.event_type) {
 
 
                 // Body detection
             case DEVICE_CONTROL_EVENT_BODY_DETECTION_ENABLED:
-                config.body_detection_enabled = true;
+                _config.body_detection_enabled = true;
                 write_nvs_config_body_detection_enabled(true);
                 break;
             case DEVICE_CONTROL_EVENT_BODY_DETECTION_DISABLED:
-                config.body_detection_enabled = false;
+                _config.body_detection_enabled = false;
                 write_nvs_config_body_detection_enabled(false);
                 break;
             case DEVICE_CONTROL_EVENT_BODY_DETECTION_DELAY_CHANGED:
-                config.body_detection_delay_seconds = event.body_detection_delay_seconds;
+                _config.body_detection_delay_seconds = event.body_detection_delay_seconds;
                 write_nvs_config_body_detection_delay(event.body_detection_delay_seconds);
-                body_detection_delay_timer_delay = config.body_detection_delay_seconds * 1000 / portTICK_PERIOD_MS;
+                _body_detection_delay_timer_delay = _config.body_detection_delay_seconds * 1000 / portTICK_PERIOD_MS;
                 break;
             case DEVICE_CONTROL_EVENT_BODY_DETECTION_TRIGGERED:
             {
                 int detected = get_body_detected();
-                if (config.body_detection_enabled) {
+                if (_config.body_detection_enabled) {
                     if (detected) {
-                        body_detected = true;
+                        _body_detected = true;
                     } else {
                         // body has gone. start timer
-                        xTimerChangePeriod(body_detection_delay_timer, body_detection_delay_timer_delay, portMAX_DELAY);
+                        xTimerChangePeriod(_body_detection_delay_timer, _body_detection_delay_timer_delay, portMAX_DELAY);
                     }
                 }
                 break;
@@ -136,15 +135,15 @@ static void device_control_task(void* arg)
 static void read_config_from_nvs()
 {
     esp_err_t err;
-    ESP_ERROR_CHECK(nvs_open("config", NVS_READWRITE, &nvs_config_handle));
+    ESP_ERROR_CHECK(nvs_open("config", NVS_READWRITE, &_nvs_config_handle));
 
     uint8_t u8bodydet_enabled;
 
-    err = nvs_get_u8(nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_ENABLED, &u8bodydet_enabled);
-    config.body_detection_enabled = u8bodydet_enabled;
+    err = nvs_get_u8(_nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_ENABLED, &u8bodydet_enabled);
+    _config.body_detection_enabled = u8bodydet_enabled;
     if (err != ESP_OK) {
         ESP_LOGE(LOG_TAG_DEVICE_CONTROL, "failed to read NVS config fan body detection enabled: %s", esp_err_to_name(err));
-        config.body_detection_enabled = BODY_DETECTION_DEFAULT_ENABLED;
+        _config.body_detection_enabled = BODY_DETECTION_DEFAULT_ENABLED;
         if (err == ESP_ERR_NVS_NOT_FOUND) {
             write_nvs_config_body_detection_enabled(BODY_DETECTION_DEFAULT_ENABLED);
         }
@@ -152,15 +151,15 @@ static void read_config_from_nvs()
         ESP_LOGI(LOG_TAG_DEVICE_CONTROL, "config - body detection: %s", u8bodydet_enabled ? "enabled" : "disabled");
     }
 
-    err = nvs_get_u32(nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_DELAY, &config.body_detection_delay_seconds);
+    err = nvs_get_u32(_nvs_config_handle, NVS_KEY_CONFIG_BODY_DETECTION_DELAY, &_config.body_detection_delay_seconds);
     if (err != ESP_OK) {
         ESP_LOGE(LOG_TAG_DEVICE_CONTROL, "failed to read NVS config body detection delay: %s", esp_err_to_name(err));
-        config.body_detection_delay_seconds = BODY_DETECTION_DEFAULT_DELAY_SECONDS;
+        _config.body_detection_delay_seconds = BODY_DETECTION_DEFAULT_DELAY_SECONDS;
         if (err == ESP_ERR_NVS_NOT_FOUND) {
             write_nvs_config_body_detection_delay(BODY_DETECTION_DEFAULT_DELAY_SECONDS);
         }
     } else {
-        ESP_LOGI(LOG_TAG_DEVICE_CONTROL, "config - body detection delay: %d", config.body_detection_delay_seconds);
+        ESP_LOGI(LOG_TAG_DEVICE_CONTROL, "config - body detection delay: %d", _config.body_detection_delay_seconds);
     }
 }
 
@@ -168,15 +167,15 @@ void init_device_control_logic()
 {
     read_config_from_nvs();
 
-    body_detection_delay_timer_delay = config.body_detection_delay_seconds * 1000 / portTICK_PERIOD_MS;
-    body_detection_delay_timer = xTimerCreate("body_detection_timer",
-                                                 body_detection_delay_timer_delay,
+    _body_detection_delay_timer_delay = _config.body_detection_delay_seconds * 1000 / portTICK_PERIOD_MS;
+    _body_detection_delay_timer = xTimerCreate("body_detection_timer",
+                                                 _body_detection_delay_timer_delay,
                                                  pdFALSE,
                                                  ( void * ) 0,
                                                  body_detection_delay_timeout
                                                  );
 
-    device_control_event_queue = xQueueCreate(20, sizeof (device_control_event));
+    _device_control_event_queue = xQueueCreate(20, sizeof (device_control_event));
 }
 
 void start_device_control_logic()
@@ -186,5 +185,5 @@ void start_device_control_logic()
 
 void device_control_send_event(device_control_event *event)
 {
-    xQueueSend(device_control_event_queue, event, portMAX_DELAY);
+    xQueueSend(_device_control_event_queue, event, portMAX_DELAY);
 }
